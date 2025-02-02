7203a11eb7593ffc88e8455315dcb3021c2e2eca
Winston <winston.h.chou@gmail.com>
2025-02-01 21:13:09 -0800

    fix example scripts

--- src/rotplot.py
+++ src/rotplot.py
@@ -45,12 +45,12 @@ def rotplot(R,currentAxes=None):
 	return ax
 
 
-
-# Example usage: Putting two rotations on one graph.
-REye = np.eye(3)
-myAxis = rotplot(REye)
-RTurn = np.array([[np.cos(np.pi/2),0,np.sin(np.pi/2)],[0,1,0],[-np.sin(np.pi/2),0,np.cos(np.pi/2)]])
-rotplot(RTurn,myAxis)
-plt.show()
+if __name__=="__main__":
+	# Example usage: Putting two rotations on one graph.
+	REye = np.eye(3)
+	myAxis = rotplot(REye)
+	RTurn = np.array([[np.cos(np.pi/2),0,np.sin(np.pi/2)],[0,1,0],[-np.sin(np.pi/2),0,np.cos(np.pi/2)]])
+	rotplot(RTurn,myAxis)
+	plt.show()
 
 
